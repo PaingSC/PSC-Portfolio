1a6c04ccd754b0cc9b72842edf9253d48f59e685
PaingSC <paingsoechit.job@gmail.com>
2024-02-21 23:44:01 +0630

    Updating <About />

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,26 @@
+import styles from "./About.module.css";
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h3>
+        About
+        <span>Introduction</span>
+      </h3>
+      <div className={styles.introduction}>
+        <p>
+          {`Hello! I'm Paing Soe Chit, a front-end web developer from [Your
+          City/Country]. Fascinated by the intersection of design and
+          technology, I've dedicated myself to crafting immersive digital
+          experiences that blend creativity with functionality. With expertise
+          in HTML, CSS, JavaScript, and React.js, I prioritize clean code and
+          user-centric design principles in every project. Beyond coding, I find
+          inspiration in nature, photography, and literature. Looking forward,
+          I'm eager to collaborate with innovative teams and organizations,
+          leveraging technology to shape the future of the web. Let's connect
+          and explore the possibilities together!`}
+        </p>
+      </div>
     </div>
   );
 }
