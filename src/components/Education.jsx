5715f6eb6599667c8e6ad48796c114aa92851ba4
PaingSC <paingsoechit.job@gmail.com>
2024-02-25 22:46:13 +0630

    Creating: <Education />

---
+++ src/components/Education.jsx
@@ -0,0 +1,11 @@
+import styles from "./Education.module.css";
+
+function Education() {
+  return (
+    <div className={styles.eduaction}>
+      <h4>Education</h4>I learn so far.
+    </div>
+  );
+}
+
+export default Education;
