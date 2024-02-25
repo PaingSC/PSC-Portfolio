013e87961740ce5c521a9cfe74b6e4cb15fd9866
PaingSC <paingsoechit.job@gmail.com>
2024-02-25 22:48:20 +0630

    Branch: Dev for development process

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,7 +3,7 @@ import styles from "./Education.module.css";
 function Education() {
   return (
     <div className={styles.eduaction}>
-      <h4>Education</h4>I learn so far.
+      <h4>Education</h4>I learn so far. Added a development branch.
     </div>
   );
 }
